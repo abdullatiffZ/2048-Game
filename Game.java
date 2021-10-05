f365fc64ea257c9f4d1bc68d2580e229f620c384
PsSarvna-16 <91929946+abdullatiffZ@users.noreply.github.com>
2021-10-05 12:29:31 +0530

    Creaed Grid Class

--- Game.java
+++ Game.java
@@ -2,5 +2,21 @@ class Game{
 	
 	public static void main(String args[]){
 
+		Grid grid = new Grid();
+		grid.show();
+	}
+}
+
+class Grid{
+
+	int[][] matrix = new int[4][4];
+
+	public void show(){
+		for(int row = 0; row < 4 ; row++){
+			for(int col = 0; col < 4 ; col++){
+				System.out.print(matrix[row][col] + "  ");
+			}
+			System.out.println();
+		}
 	}
 }
